8a2c2bbffa9659760ce8ec233dc4bc61416ddc60
eklogvinov <eklogvinov@chel.rucard.ru>
2017-05-26 13:24:29 +0500

    add buttons and themes

--- gradle-spring-mvc/src/main/webapp/static/js/controllers/task.js
+++ gradle-spring-mvc/src/main/webapp/static/js/controllers/task.js
@@ -1,6 +1,33 @@
-angular.module('tasksApp', []).controller('taskController',
-		function($scope, $http) {
-			$http.get('/gradle-spring-mvc/task').then(function(response) {
-				$scope.task = response.data;
-			});
-		});
+angular
+		.module('tasksApp', [ 'ngAnimate', 'ngMaterial' ])
+		.config(
+				function($mdThemingProvider) {
+					$mdThemingProvider.theme('default').primaryPalette('pink')
+							.accentPalette('orange');
+				})
+		.controller(
+				'taskController',
+				function($scope, $http) {
+					$http.get('/gradle-spring-mvc/task').then(
+							function(response) {
+								$scope.task = response.data;
+							});
+					$scope.user = {
+						title : 'Developer',
+						email : 'ipsum@lorem.com',
+						firstName : '',
+						lastName : '',
+						company : 'Google',
+						address : '1600 Amphitheatre Pkwy',
+						city : 'Mountain View',
+						state : 'CA',
+						biography : 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+						postalCode : '94043'
+					};
+
+					$scope.title1 = 'Button';
+					$scope.title4 = 'Warn';
+					$scope.isDisabled = true;
+
+					$scope.googleUrl = 'http://google.com';
+				});
